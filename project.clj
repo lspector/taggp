caf19b6ec4ce55d1d1ed2caf831b1ad2a3d54b3c
Lee Spector <lspector@hampshire.edu>
2012-01-23 21:31:34 -0500

    Version bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject taggp "0.0.4"
+(defproject taggp "0.0.5"
   :description "Tree-based genetic programming with tagging."
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.3.0"]])
